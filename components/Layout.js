b6c9b3c88de25bfa88a243816b807a27bb20aa7c
yuicheng1006 <yuice1006@gmail.com>
2021-03-29 14:00:40 +0800

    fix photo

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [showMenu,toggleShowMenu]=useState(false);
   return (
     <div className={showMenu?"out-wrap":null} onClick={showMenu?()=>toggleShowMenu(false):null}>
+      <div className="mobile-nav"></div>
       <About showMenu={showMenu}/>
       <button className="btn show-menu-btn" onClick={()=>toggleShowMenu(true)}>About</button>
       {showMenu&&(<button className="btn close-menu-btn" onClick={()=>toggleShowMenu(!showMenu)}>CLOSE</button>)}
