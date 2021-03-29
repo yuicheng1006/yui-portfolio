b25a59f5cf7e1f060d585753e3698087e9e6042e
yuicheng1006 <yuice1006@gmail.com>
2021-03-29 14:52:50 +0800

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/main.scss';
 import "aos/dist/aos.css";
+import { initGA, logPageView } from '../utils/analytics'
 
 function MyApp({ Component, pageProps }) {
+  componentDidMount= ()=> {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView();
+  }
   return <Component {...pageProps} />
 }
 
