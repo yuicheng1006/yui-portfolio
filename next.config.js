b450efc88e41396297eb543525487b51e7ad2ace
Yui Cheng <yuice1006@gmail.com>
2025-06-18 21:40:10 +0800

    [master] test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       ...config.resolve.fallback,
+//       fs: false,
+//     };
+//     return config;
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  presets: ['next/babel'],
+  // 移除或註解掉這個 webpack 配置
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback,
+  //       fs: false,
+  //     };
+  //   }
+  //   return config;
+  // },
+};
+
+module.exports = nextConfig;
